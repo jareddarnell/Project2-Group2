8e84db74f3dc76e2d691c881c60b0dbd5f34a128
jareddarnell <43456129+jareddarnell@users.noreply.github.com>
2018-10-03 21:55:11 -0600

    Create SQLQUERIES.sql

---
+++ SQLQUERIES.sql
@@ -0,0 +1,7 @@
+/* This file is for the sql queries */
+
+--Query that returns all sites in a campground, with all amenities, and availability for each site
+
+--Query that returns how many days each spot in the campground was reserved for in a given date range with a percentage of utlization
+
+--Query that returns revenue generated for each campground by month for the months of: June, July, August, and September
