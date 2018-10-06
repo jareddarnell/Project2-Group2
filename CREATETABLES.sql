2e235d8bf484857f9a67ea18095d3f0ef15811d0
Jared Darnell <jareddarnell@mail.weber.edu>
2018-10-05 22:35:49 -0600

    Initial table template

--- CREATETABLES.sql
+++ CREATETABLES.sql
@@ -2,6 +2,107 @@
 
 --Table Creation
 
+CREATE TABLE [Activities]
+(
+	[ActivitiesID] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[CampgroundID] INT NOT NULL,
+	[ATVRiding] BIT NOT NULL DEFAULT 0,
+	[Fishing] BIT NOT NULL DEFAULT 0,
+	[Hiking] BIT NOT NULL DEFAULT 0,
+	[Boating] BIT NOT NULL DEFAULT 0,
+	[Swimming] BIT NOT NULL DEFAULT 0,
+	[Hunting] BIT NOT NULL DEFAULT 0,
+	[HorseBackRiding] BIT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE [Campgrounds]
+(
+	[CampgroundID] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[Name] VARCHAR(50) NOT NULL,
+	[Location] VARCHAR(50) NOT NULL,	--Lat-Long possibly use non-euclidian spatial data type
+	[Climate] VARCHAR(20) NOT NULL,
+	[ExtraParkingSpots] INT NOT NULL,
+	[VaultToilets] BIT NOT NULL,
+	[FlushToilets] BIT NOT NULL,
+	[AvailableStartDate] DATE NOT NULL,
+	[AvailableEndDate] DATE NOT NULL
+);
+
+CREATE TABLE [Sites]
+(
+	[SiteID] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[CampgroundID] INT NOT NULL,
+	[SiteNumber] INT NOT NULL,
+	[Functional] BIT NOT NULL DEFAULT 1,
+	[Price] MONEY NOT NULL,
+	[ParkingSpots] INT NOT NULL,
+	[Capacity] INT NOT NULL,
+	[Tent] BIT NOT NULL DEFAULT 0,
+	[Trailer] BIT NOT NULL DEFAULT 0,
+	[WaterAccess] BIT NOT NULL DEFAULT 0,
+	[Firepit] BIT NOT NULL DEFAULT 0,
+	[Table] BIT NOT NULL DEFAULT 0,
+	[Covered] BIT NOT NULL DEFAULT 0,
+	[Shade] BIT NOT NULL DEFAULT 0,
+	[Picture] VARBINARY(MAX) --https://www.c-sharpcorner.com/article/how-should-we-store-images-and-blob-files-in-sql-server/
+);
+
+CREATE TABLE [Holidays]
+(
+	[HolidayID] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[CampgroundID] INT NOT NULL,
+	[Name] VARCHAR(50) NOT NULL,
+	[StartDate] DATE NOT NULL,
+	[EndDate] DATE NOT NULL
+);
+
+CREATE TABLE [Users]
+(
+	[UserID] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[CampgroundID] INT NOT NULL,
+	[FirstName] VARCHAR(20) NOT NULL,
+	[LastName] VARCHAR(20) NOT NULL,
+	[CellPhoneNumber] VARCHAR(15) NOT NULL,
+	[Email] VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE [Reservations]
+(
+	[ReservationID] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[UserID] INT NOT NULL, 
+	[SiteID] INT NOT NULL,
+	[StartDate] DATE NOT NULL,
+	[EndDate] DATE NOT NULL
+);
+
+CREATE TABLE [Hosts]
+(
+	[HostID] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[CampgroundID] INT NOT NULL,
+	[FirstName] VARCHAR(20) NOT NULL,
+	[LastName] VARCHAR(20) NOT NULL,
+	[CellPhoneNumber] VARCHAR(15) NOT NULL
+);
+
 --Table Alteration
+
+ALTER TABLE [Activities]
+ADD FOREIGN KEY ([CampgroundID]) REFERENCES [Campgrounds]([CampgroundID])
+
+ALTER TABLE [Sites]
+ADD FOREIGN KEY ([CampgroundID]) REFERENCES [Campgrounds]([CampgroundID])
+
+ALTER TABLE [Holidays]
+ADD FOREIGN KEY ([CampgroundID]) REFERENCES [Campgrounds]([CampgroundID])
+
+ALTER TABLE [Users]
+ADD FOREIGN KEY ([CampgroundID]) REFERENCES [Campgrounds]([CampgroundID])
+
+ALTER TABLE [Reservations]
+ADD FOREIGN KEY ([SiteID]) REFERENCES [Sites]([SiteID]),
+	FOREIGN KEY ([UserID]) REFERENCES [Users]([UserID])
+
+ALTER TABLE [Hosts]
+ADD FOREIGN KEY ([CampgroundID]) REFERENCES [Campgrounds]([CampgroundID])
 
 --Data Insertion
