90ef968999cff39b4d0e3480757e490b824c8329
zecher <quintondixon@gmail.com>
2018-10-10 12:51:44 -0600

    Added insert for one user and a Madison Campground reservation for them

--- SQLQUERIES.sql
+++ SQLQUERIES.sql
@@ -7,8 +7,38 @@ CS 3550
 /* This file is for the sql queries */
 
 -- Insert statements for 2 new users of your system.
+SET IDENTITY_INSERT [Users] ON;
+
+INSERT INTO [Users]
+(
+	[UserID],
+	[CampgroundID],
+	[FirstName],
+	[LastName],
+	[CellPhoneNumber],
+	[Email]
+)
+VALUES
+	('7','2','Tim','Taylor','801-123-4567','toolmantaylor@yahoo.com'); --Madison Campground
+
+SET IDENTITY_INSERT [Users] OFF;
 
 -- Insert statements for reservations by these two new people for 4 day stays at one of your campgrounds.
+SET IDENTITY_INSERT [Reservations] ON;
+
+INSERT INTO [Reservations]
+(
+	[ReservationID],
+	[UserID],
+	[SiteID],
+	[StartDate],
+	[EndDate],
+	[TotalPrice]
+)
+VALUES
+	('7','7','14','03-Jul-18','06-Jul-18','101.00'); --Madison Campground
+
+SET IDENTITY_INSERT [Reservations] OFF;
 
 --Query that returns all sites in a campground, with all amenities, and availability for each site
 
